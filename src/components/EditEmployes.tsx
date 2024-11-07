8e74a10c00f0a40866e71e30ae603cb1104e72ed
guilhermeosr <guilherme.o.santarosa@gmail.com>
2024-11-07 18:58:50 -0300

    Add botao para download pdf

--- src/components/EditEmployes.tsx
+++ src/components/EditEmployes.tsx
@@ -394,26 +394,26 @@ const EditEmployes: React.FC  = () => {
                                 />
 
                                 <Button type="submit" variant="contained" color="primary">Salvar</Button>
-                                {pdfBlob && (
+                      
                             <PDFDownloadLink
                                 document={<PDFDocumentComponentEdit employeeData={employee} />}
                                 fileName={`${employee?.name || 'funcionario'}-dados.pdf`}
                                 style={{ textDecoration: 'none', marginTop: '20px', display: 'block' }}
-                            >
+                                >
                                  <Button
-                                    sx={{ 
-                                    marginTop: '5px', 
-                                    width: '100%', 
-                                    backgroundColor: '#6A0DAD', 
-                                    color: '#FFF', 
-                                    '&:hover': { backgroundColor: '#5A0C8B' } 
-                                    }}
-                                    variant="contained"
-                                    fullWidth
+                                        sx={{ 
+                                        marginTop: '5px', 
+                                        width: '100%', 
+                                        backgroundColor: '#6A0DAD', 
+                                        color: '#FFF', 
+                                        '&:hover': { backgroundColor: '#5A0C8B' } 
+                                        }}
+                                        variant="contained"
+                                        fullWidth
 
-                                >Baixar</Button>
+                                    >Baixar</Button>
                             </PDFDownloadLink>
-                        )}
+                     
                             </form>
                         </Grid>
                         <Grid item xs={12} md={6}>
